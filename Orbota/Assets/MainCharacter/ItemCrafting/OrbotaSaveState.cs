cb325b2c448f3d5b889311b77ddf283fab949cab
Cooper Redfern <coopr97@gmail.com>
2019-10-14 08:14:23 -0500

    Corgi removed

--- Orbota/Assets/MainCharacter/ItemCrafting/OrbotaSaveState.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "SaveState", menuName = "Abstracts/SaveState", order = 2)]
-public class OrbotaSaveState : OrbotaAbstractObject
-{
-    
-}
